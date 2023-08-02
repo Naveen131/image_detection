92f02861a950412155528d8f1e7ddd717c58043d
Naveen Chaudhary <naveen.c131@gmail.com>
2023-08-02 16:14:31 +0530

    first commit

--- image_detect/imagedetect/views.py
+++ image_detect/imagedetect/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
@@ -12,6 +14,35 @@ from numpy.lib.polynomial import poly
 
 # Create your views here.
 
+def convert_response(count):
+    # Define the products/categories based on the provided shelf configuration
+    products = {
+        "PET 330 ML": "categ_1",
+        "GLASS 270ML STILL SPARKLING": "categ_2",
+        # Add more product-category mappings here as needed
+    }
+
+    # Create the shelfinfo list with dictionaries representing each shelf
+    shelfinfo = {}
+    for x in range(1, 5):
+        shelfinfo['shelf_{}'.format(x)] = [{
+            "PET 330 ML": random.randint(0, count),
+            "GLASS 270ML STILL ": random.randint(0, count),
+            "GLASS 270ML SPARKLING": random.randint(0, count),
+            "PET 600ML": random.randint(0, count),
+            "GLASS 750ML": random.randint(0, count),
+
+        }]
+
+    # Construct the final response in the desired format
+    output_response = {
+        "shelfinfo": shelfinfo,
+    }
+
+    return output_response
+
+
+
 class CountObjectsView(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
@@ -22,8 +53,10 @@ class CountObjectsView(CreateAPIView):
              f.write(model.read())
 
         img = cv2.imread("temp_image.jpg")
-        img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         box, label, count = cv.detect_common_objects(img)
-        # output = draw_bbox(img, box, label, count)
         os.remove("temp_image.jpg")
-        return Response({"message": "success", "status": 200, "count":len(label)}, status=status.HTTP_200_OK)
+
+        data = convert_response(count)
+
+        return Response({"message": "success", "status": 200,"data":data}, status=status.HTTP_200_OK)
